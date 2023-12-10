a4d72dc9f4b0e0c6a1329d598a599b681ab5e6e6
Benjythebee <Benjy.larcher@aol.com>
2023-12-11 10:13:26 +1300

    fix query

--- src/api.ts
+++ src/api.ts
@@ -79,14 +79,13 @@ export default function APIRouter(app: express.Application) {
         }
 
         const page = req.query?.page || 0
-        console.log('_only', _only)
-        console.log('cleanOnlyAddresses', cleanOnlyAddresses)
+
         // Query NFTs for this NFT
         let query = `SELECT t.*,c.blockchain,c.address as contract_address FROM token_ownership t JOIN contract c ON t.contract_id = c.contract_id WHERE t.owner = decode($1,'hex')`;
         if(cleanExclude.length) {
-            `SELECT t.*,c.blockchain,c.address as contract_address FROM token_ownership t JOIN contract c ON t.contract_id = c.contract_id WHERE t.owner = decode($1,'hex') and encode(c.address,'hex') not in (${cleanExclude.map((a)=>`'${a}'`).join(', ')}) `;
+            query=`SELECT t.*,c.blockchain,c.address as contract_address FROM token_ownership t JOIN contract c ON t.contract_id = c.contract_id WHERE t.owner = decode($1,'hex') and encode(c.address,'hex') not in (${cleanExclude.map((a)=>`'${a}'`).join(', ')}) `;
         }else if (cleanOnlyAddresses.length) {
-            `SELECT t.*,c.blockchain,c.address as contract_address FROM token_ownership t JOIN contract c ON t.contract_id = c.contract_id WHERE t.owner = decode($1,'hex') and encode(c.address,'hex') in (${cleanOnlyAddresses.map((a)=>`'${a}'`).join(', ')}) `;
+            query=`SELECT t.*,c.blockchain,c.address as contract_address FROM token_ownership t JOIN contract c ON t.contract_id = c.contract_id WHERE t.owner = decode($1,'hex') and encode(c.address,'hex') in (${cleanOnlyAddresses.map((a)=>`'${a}'`).join(', ')}) `;
         }
         if (chain) {
             query += ` AND c.blockchain = $3::blockchain`;
