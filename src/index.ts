dfe499b39738d577644fc96308e70f5a6de1ce93
Benjythebee <Benjy.larcher@aol.com>
2024-02-22 07:13:35 +1300

    extended: true

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ import APIRouter from "./api";
 
 const app = express();
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true}));
 app.use(cors({
   exposedHeaders: ['*', 'x-paradigm-secret'],
   allowedHeaders: ['*', 'x-paradigm-secret'],
