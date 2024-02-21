1580d1f22f6625e0e4630949951296d59b78824a
Benjythebee <Benjy.larcher@aol.com>
2024-02-22 07:10:54 +1300

    add limit 50mb

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,8 @@ import { LogDescription } from "ethers/lib/utils";
 import APIRouter from "./api";
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors({
   exposedHeaders: ['*', 'x-paradigm-secret'],
   allowedHeaders: ['*', 'x-paradigm-secret'],
@@ -216,4 +217,4 @@ setTimeout(async () => {
 
 setInterval(async () => {
   console.log('memoryHeap: ' + heapStats().heapSize / 1024 / 1024 + 'mb')
-}, 10000)
+}, 30000)
